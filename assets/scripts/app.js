5ce4c359a33af38b9e3e305954cde79d90818fc7
jmk <jmkolata@gmail.com>
2022-01-01 08:18:48 -0500

    forgot the js file

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + parseInt(enteredNumber);
+  outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
