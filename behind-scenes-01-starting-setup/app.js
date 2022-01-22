d67aca96bd0c6c4c844578e0feaf0cb7b51039cd
jmk <jmkolata@gmail.com>
2022-01-22 12:07:38 -0500

    example of hoisting

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,21 +1,26 @@
-//using var lets you redeclare, this isn't desirable
-let myname = 'jmk';
+// //using var lets you redeclare, this isn't desirable
+// let myname = 'jmk';
 
-if (myname === 'jmk') {
-  //since this var isn't in a function, it's a global
-  //using let or const would scope it to this block
-  var hobbies = ['cheam', 'bike'];
-  console.log(hobbies);
-}
+// if (myname === 'jmk') {
+//   //since this var isn't in a function, it's a global
+//   //using let or const would scope it to this block
+//   var hobbies = ['cheam', 'bike'];
+//   console.log(hobbies);
+// }
 
-function greet() {
-  let age = 32;
-  //this is called shadowing
-  //inner declaration overrides outer/global declaration
-  let myname = 'monke'
-  console.log(myname, age);
-}
+// function greet() {
+//   let age = 32;
+//   //this is called shadowing
+//   //inner declaration overrides outer/global declaration
+//   let myname = 'monke'
+//   console.log(myname, age);
+// }
 
-greet();
+// greet();
 
-console.log(myname, hobbies);
+// console.log(myname, hobbies);
+
+
+// example of hoisting, "let" and "const" prevent this
+console.log(userName);
+var userName = 'JMK';
