2f60fe9f2b163f8bd61e5c7ce0cfba5d6f9b1f6d
jmk <jmkolata@gmail.com>
2022-01-22 12:03:08 -0500

    var and let explained

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,21 @@
+//using var lets you redeclare, this isn't desirable
+let myname = 'jmk';
+
+if (myname === 'jmk') {
+  //since this var isn't in a function, it's a global
+  //using let or const would scope it to this block
+  var hobbies = ['cheam', 'bike'];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 32;
+  //this is called shadowing
+  //inner declaration overrides outer/global declaration
+  let myname = 'monke'
+  console.log(myname, age);
+}
+
+greet();
+
+console.log(myname, hobbies);
