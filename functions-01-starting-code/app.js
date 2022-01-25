802d2dddb827a6f01d0c4df727fb9f66ee4b6d23
jmk <jmkolata@gmail.com>
2022-01-24 20:59:13 -0500

    adding some simple game logic

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,18 +1,32 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function() {
-  console.log('Game is starting...');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}`, '').toUpperCase();
+  if (
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    alert(`Invalid choice, we chose ${DEFAULT_USER_CHOICE} for you`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
 
-// const person = {
-//   greet: function greet() {
-//     console.log('hello there');
-//   }
-// };
-
-// person.greet();
-
 //anonymous function
-startGameBtn.addEventListener('click', function() {
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
