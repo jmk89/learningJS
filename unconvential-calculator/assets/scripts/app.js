70f2e95de857e53f3515ed875b82e585aa3d8138
jmk <jmkolata@gmail.com>
2022-01-26 21:55:46 -0500

    added bind() to calculator

--- unconvential-calculator/assets/scripts/app.js
+++ unconvential-calculator/assets/scripts/app.js
@@ -24,56 +24,29 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newResult)
   console.log(logEntries);
 }
 
-function calculateResult(calculationType) {
-  const num = getUserNumberInput();
-  if (
-    calculationType !== 'ADD' &&
-    calculationType !== 'SUBTRACT' &&
-    calculationType !== 'MULTIPLY' && 
-    calculationType !== 'DIVIDE' ||
-    !num
-  ) {
-    return;
-  }
-
+function calculate(operation) {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   let mathOperator;
-  if (calculationType === 'ADD') {
+  if (operation === 'ADD') {
     currentResult += enteredNumber;
     mathOperator = '+';
-  } else if (calculationType === 'SUBTRACT') {
+  } else if (operation === 'SUBTRACT') {
     currentResult -= enteredNumber;
     mathOperator = '-';
-  } else if (calculationType === 'MULTIPLY') {
+  } else if (operation === 'MULTIPLY') {
     currentResult *= enteredNumber;
     mathOperator = '*';
-  } else if (calculationType === 'DIVIDE') {
+  } else if (operation === 'DIVIDE') {
     currentResult /= enteredNumber;
     mathOperator = '/';
   }
-  
+
   createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-function add() {
-  calculateResult('ADD');
-}
-
-function subtract() {
-  calculateResult('SUBTRACT');
-}
-
-function multiply() {
-  calculateResult('MULTIPLY');
-}
-
-function divide() {
-  calculateResult('DIVIDE');
-}
-
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener('click', calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculate.bind(this, 'DIVIDE'));
