3c70a473cee8f53f9376fa194a569a7be214a6d1
jmk <jmkolata@gmail.com>
2022-02-03 22:31:32 -0500

    some starting stuff for DOM manipulation section

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,11 @@
+//selects the body node
+// document.body 
+//selects the head element node
+// document.head
+//selects the html element node
+// document.documentElement
+
+// document.querySelector('.list-item');
+// document.querySelectorAll('.list-item');
+
+// document.querySelector('ul li:first-of-type');
