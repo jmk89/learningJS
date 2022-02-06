c531aef880088884e3e930c7ff7f0071f9c86767
jmk <jmkolata@gmail.com>
2022-02-05 20:08:30 -0500

    some more DOM manipulation and short assignment

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -9,3 +9,20 @@
 // document.querySelectorAll('.list-item');
 
 // document.querySelector('ul li:first-of-type');
+
+const h1 = document.getElementById('main-title');
+h1.textContent = 'Some new title';
+h1.style.color = 'white';
+h1.style.backgroundColor = 'dodgerBlue';
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (changed)'
+
+const body = document.body;
+body.query
+
+const listItemElements = document.querySelectorAll('li');
+
+for (const listItem of listItemElements) {
+  console.dir(listItem)
+}
---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,13 @@
+const li = document.querySelector('h1');
+const li2 = document.getElementById('task-1')
+
+li2.style.backgroundColor = 'black';
+li2.style.color = 'white';
+
+const head = document.querySelector('head');
+const title = head.querySelector('title')
+title.innerText = 'test'
+console.dir(title)
+
+const h1 = document.querySelector('h1');
+h1.textContent = "Solved"
