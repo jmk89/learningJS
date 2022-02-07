e86c6807f1f4b63f97bfa60f52f25cf95e10d059
jmk <jmkolata@gmail.com>
2022-02-06 21:40:19 -0500

    showing a way to traverse the DOM

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -1,27 +1,5 @@
-//selects the body node
-// document.body 
-//selects the head element node
-// document.head
-//selects the html element node
-// document.documentElement
+// a way to get the ul
+const ul = document.body.firstElementChild.nextElementSibling
+const firstLi = ul.firstElementChild;
 
-// document.querySelector('.list-item');
-// document.querySelectorAll('.list-item');
-
-// document.querySelector('ul li:first-of-type');
-
-const h1 = document.getElementById('main-title');
-h1.textContent = 'Some new title';
-h1.style.color = 'white';
-h1.style.backgroundColor = 'dodgerBlue';
-
-const li = document.querySelector('li:last-of-type');
-li.textContent = li.textContent + ' (changed)'
-
-const body = document.body;
-
-const listItemElements = document.querySelectorAll('li');
-
-// for (const listItem of listItemElements) {
-//   console.dir(listItem)
-// }
+console.log(firstLi)
