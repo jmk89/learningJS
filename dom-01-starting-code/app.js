07b98f9a7f0d58cf4a533ec75ccca1532b7e686b
jmk <jmkolata@gmail.com>
2022-02-07 11:00:15 -0500

    inserting DOM elements

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -1,6 +1,13 @@
 const list = document.querySelector('ul');
 const newLi = document.createElement('li');
+const anotherLi = document.createElement('li');
 
-list.appendChild(newLi);
 newLi.textContent = 'item 4';
 newLi.style.backgroundColor = 'blue'
+list.prepend(newLi);
+
+list.lastElementChild.before(anotherLi)
+list.firstElementChild.replaceWith(anotherLi)
+
+const secondLi = list.children[1];
+secondLi.insertAdjacentElement('afterend', newLi)
